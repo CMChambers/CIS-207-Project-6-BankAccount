13d3926ca23ef97f6e4d96a7d5f764a59562dfcb
Chad M Chambers <61452826+CMChambers@users.noreply.github.com>
2025-10-29 23:40:36 -0400

    add/ basics in program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace CIS207.Project6BankAccount
+{
+    class BankAccount
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Bank Account ===");
+
+            BankAccount account1;
+            BankAccount account2;
+
+            try
+            {
+                account1 = new BankAccount("savings account", 100);
+                account2 = new BankAccount("spending account", 500);
+
+                account1.PrintSummary();
+                account2.PrintSummary();
+
+                try
+                {
+                    account1.Deposit(30);
+                    account2.Deposit(100);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                try
+                {
+                    account1.Withdraw(5);
+                    account2.Withdraw(80);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                account1.PrintSummary();
+            }
+            catch
+            {
+
+            }
+
+        }
+    }
+}
